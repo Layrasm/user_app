eb212fa82212907fcd2fd30bbf65c27cf03fcc00
Layne Rasmussen <layrasm@gmail.com>
2020-08-26 07:52:59 -0600

    users controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,54 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy] #this is loading controller and setting the variable (in private method) for each action
+  
+  
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    @user #could delete this so code is DRY
   end
 
   def new
-    @user = User.new(user_params)
+    @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+
   def edit
+    @user #could delete
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+      redirect_to users_path
   end
 
   private
-    def user_params
-      params.require(:user).permit()
+    def set_user
+      @user = User.find(params[:id])
+    end
 
-  def delete
-  end
+    def user_params
+
+      params.require(:user).permit(:first_name, :last_name, :age, :hair_color, :eye_color, :gender, :alive)
+    end
 end
